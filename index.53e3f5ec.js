2a070a5c655437a9ba179990020deadc51dea6f2
Nierochron <166538574+Nierochron@users.noreply.github.com>
2026-01-15 18:38:58 +0000

    Deploying to gh-pages from @ Nierochron/event-project@5392948b63346c0c7627d23f31fdfce0b132a2f6 🚀

---
+++ index.53e3f5ec.js
@@ -0,0 +1,2 @@
+fetch("".concat("https://app.ticketmaster.com/discovery/v2/","events.json?apikey=").concat("PGVpj5B2ke0spXMDy1QJDYveXLa7zjEE")).then((function(t){if(!t.ok)throw new Error(t.status);return t.json()})).then((function(t){console.log(t)})).catch((function(t){console.error(t)}));
+//# sourceMappingURL=index.53e3f5ec.js.map
